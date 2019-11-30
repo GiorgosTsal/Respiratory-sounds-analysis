fd5ee2877b748c197118dccbb62a803d4b07c87e
gtsal <tsalgiwrgos@gmail.com>
2019-11-30 20:21:31 +0200

    create an audio spectrogram

--- parse_data.py
+++ parse_data.py
@@ -21,14 +21,42 @@ from pydub.playback import play
 import warnings
 warnings.filterwarnings('ignore')
 
+import soundfile as sf
+
+
+# Define helper functions
+
+# Load a .wav file. 
+# These are 24 bit files. The PySoundFile library is able to read 24 bit files.
+# https://pysoundfile.readthedocs.io/en/0.9.0/
+
+def get_wav_info(wav_file):
+    data, rate = sf.read(wav_file)
+    return data, rate
+
+def graph_spectrogram(wav_file):
+    data, rate = get_wav_info(wav_file)
+    nfft = 200 # Length of each window segment
+    fs = 8000 # Sampling frequencies
+    noverlap = 120 # Overlap between windows
+    nchannels = data.ndim
+    if nchannels == 1:
+        pxx, freqs, bins, im = plt.specgram(data, nfft, fs, noverlap = noverlap)
+    elif nchannels == 2:
+        pxx, freqs, bins, im = plt.specgram(data[:,0], nfft, fs, noverlap = noverlap)
+    return pxx
+
+
+
+
 get_ipython().run_line_magic('matplotlib', 'inline')
 base_path = '/home/gtsal/Desktop/Machine learning/respiratory-sound-database/Respiratory_Sound_Database/Respiratory_Sound_Database/'
 data_path = base_path + '/audio_and_txt_files/'
 dirs = os.listdir( data_path )
 
 # This would print all the files and directories
-for file in dirs:
-   print (file)
+#for file in dirs:
+  # print (file)
     
 # Install the pydub library 
 #$ conda install -c conda-forge pydub
@@ -38,7 +66,9 @@ for file in dirs:
 # 101_1b1_Al_sc_Meditron.wav
 
 audio_file = '101_1b1_Al_sc_Meditron.wav'   
+audio_file_1 = '110_1p1_Al_sc_Meditron.wav'   
 audio_path = base_path + "/audio_and_txt_files/" + audio_file
+audio_path_1 = base_path + "/audio_and_txt_files/" + audio_file_1
 
 song = AudioSegment.from_wav(audio_path)
 #play(song)
@@ -60,9 +90,11 @@ diagn_col_names = ['patient_id','diagnosis']
 
 df_demo = pd.read_csv(demographic_info, sep=" ", header=None, names=demo_col_names)
 df_demo.head(10)
+print(df_demo)
 
 df_diag = pd.read_csv(patient_diagnosis, header=None, names=diagn_col_names)
 df_diag.head(10)
+print(df_diag)
 
 file_format_data = open(filename_format, 'r').read()
 
@@ -75,8 +107,19 @@ anot_col_names = ['Beginning_of_respiratory_cycle', 'End_of_respiratory_cycle', 
 # Presence = 1
 # Absence = 0
 df_annot = pd.read_csv(annotation, sep="\t", header=None, names=anot_col_names)
-df_annot.head(10)
+df_annot.head(20)
 print(df_annot)
 
+# Install the pysoundfile library
+#$ conda install -c conda-forge pysoundfile
 
+# plot the spectrogram
+
+# Time is on the x axis and Frequencies are on the y axis.
+# The intensity of the different colours shows the amount of energy i.e. how loud the sound is,
+# at different frequencies, at different times
+
+
+both = graph_spectrogram(audio_path_1)
+healthy = graph_spectrogram(audio_path)
 
